e7f9eb68be5ed66890362ef0fac05c509a60563a
rosinekaranganwa <rosinekaranganwa@gmail.com>
2024-03-14 11:32:41 -0400

    configuration

---
+++ src/main/java/com/example/demo1/Demo1Application.java
@@ -0,0 +1,18 @@
+package com.example.demo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Demo1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo1Application.class, args);
+    }
+    public String hello(){
+        return  "Hello world";
+    }
+
+}
