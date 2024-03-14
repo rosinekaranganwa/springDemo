186ea0a46eb14c8a535a406ea64c521ae0733ee0
rosinekaranganwa <rosinekaranganwa@gmail.com>
2024-03-14 18:30:51 -0400

    CRUD operation service

--- src/main/java/com/example/demo1/Demo1Application.java
+++ src/main/java/com/example/demo1/Demo1Application.java
@@ -1,18 +1,20 @@
 package com.example.demo1;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
 @SpringBootApplication
-@RestController
 public class Demo1Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Demo1Application.class, args);
     }
-    public String hello(){
-        return  "Hello world";
-    }
+
 
 }
--- src/main/java/com/example/demo1/student/StudentService.java
+++ src/main/java/com/example/demo1/student/StudentService.java
@@ -1,4 +1,50 @@
 package com.example.demo1.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import javax.swing.text.html.Option;
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class StudentService {
+   private final StudentRepository studentRepository;
+
+   @Autowired
+    public StudentService(StudentRepository studentRepository){
+       this.studentRepository=studentRepository;
+   }
+
+   public Student createStudent(Student student){
+       return  studentRepository.save(student);
+   }
+
+   public List<Student> getAllStudents(){
+       return  studentRepository.findAll();
+   }
+
+   public Optional<Student> getStudentById(Long id){
+       return studentRepository.findById(id);
+   }
+
+   public  Student updateStudent(Long id,Student newStudent){
+       return studentRepository.findById(id).map(student -> {
+           student.setName(newStudent.getName());
+           student.setEmail(newStudent.getEmail());
+           student.setDob(newStudent.getDob());
+           return studentRepository.save(student);
+       }).orElseGet(() ->{
+           newStudent.setId(id);
+           return studentRepository.save(newStudent);
+       });
+   }
+   public void deleteStudent(Long id){
+       studentRepository.deleteById(id);
+   }
 }
+
+
